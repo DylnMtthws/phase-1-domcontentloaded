d4dff7179b7992db352ce173c3d3e168c7c42300
dylanmatthews <djmatthews93@gmail.com>
2023-09-13 15:22:40 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function () {
+   const p = document.querySelector('#text'); 
+   //declare a variable and then use querySelector to identify what you want to change
+    //THEN! .textContent = "string" to change the DOM
+   p.textContent = "This is really cool!"
+})
